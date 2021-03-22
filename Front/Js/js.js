6dd7bab534faed15c2be09fb242f1755482259fc
juan david <juan.delaparra@usa.edu.co>
2021-03-22 14:39:57 -0500

    formulario registro

--- Front/Js/js.js
+++ Front/Js/js.js
@@ -1,1 +1,39 @@
 $('.ui.dropdown').dropdown('hidden');
+
+
+
+function registro() {
+
+    //console.log("entro¡");
+    var $accountregistro = $('#bt-registrar'),
+        $accountconfirmacion = $('#registrar');
+
+    $accountregistro.on('click', function() {
+        $accountconfirmacion[0].show();
+    });
+
+    $('#cancel').on('click', function() {
+        $accountconfirmacion[0].close();
+    });
+
+};
+/*(function registro() {
+    'use strict';
+    console.log("entro¡");
+    var $accountregistro = $('#bt-registrar'),
+        $accountconfirmacion = $('#confirm-registrar');
+
+    $accountregistro.on('click', function() {
+        $accountconfirmacion[0].showModal();
+    });
+
+    $('#cancel').on('click', function() {
+        $accountDeleteDialog[0].close();
+    });
+
+});
+/*
+var dialogoPregunta = document.getElementById('registrar');
+dialogoPregunta.addEventListener('close', () =>
+    alert('Has contestado que ' + dialogoPregunta.returnValue)
+);*/
