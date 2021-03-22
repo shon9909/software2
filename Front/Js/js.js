bb9599498cebf86b4387868c336516c698f390ce
juan david <juan.delaparra@usa.edu.co>
2021-03-22 16:57:50 -0500

    vinculacion con registro

--- Front/Js/js.js
+++ Front/Js/js.js
@@ -1,34 +1,43 @@
 $('.ui.dropdown').dropdown('hidden');
 $('.ui.form')
-  .form({
-    fields: {
-        correoElectronico: {
-            identifier: 'correoElectronico',
-            rules: [
-              {
-                type   : 'empty',
-                prompt : 'Por favor ingrese su correo electrónico'
-              }
-            ]
-          },
-      contraseña: {
-        identifier: 'contraseña',
-        rules: [
-          {
-            type   : 'empty',
-            prompt : 'Por favor ingrese su contraseña'
-          },
-          {
-            type   : 'minLength[6]',
-            prompt : 'La contraseña debe ser de minimo {ruleValue} caracteres'
-          }
-        ]
-      }  
-    }
-  })
-;
+    .form({
+        fields: {
+            correoElectronico: {
+                identifier: 'correoElectronico',
+                rules: [{
+                    type: 'empty',
+                    prompt: 'Por favor ingrese su correo electrónico'
+                }]
+            },
+            contraseña: {
+                identifier: 'contraseña',
+                rules: [{
+                        type: 'empty',
+                        prompt: 'Por favor ingrese su contraseña'
+                    },
+                    {
+                        type: 'minLength[6]',
+                        prompt: 'La contraseña debe ser de minimo {ruleValue} caracteres'
+                    }
+                ]
+            }
+        }
+    });
 $('select.dropdown')
-  .dropdown()
-;
+    .dropdown();
 
+function registro() {
 
+    //console.log("entro¡");
+    var $accountregistro = $('#bt-registrar'),
+        $accountconfirmacion = $('#registrar');
+
+    $accountregistro.on('click', function() {
+        $accountconfirmacion[0].show();
+    });
+
+    $('#cancel').on('click', function() {
+        $accountconfirmacion[0].close();
+    });
+
+};
